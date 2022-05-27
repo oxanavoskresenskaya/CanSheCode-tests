0fe5cbe53be67486238c7a9c902faddb051699a8
OxanaVoskresenskaya <oavoskresenskaya@mail.ru>
2022-05-27 16:15:01 +0300

    adding button with JS

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-const listElement=document.querySelector('.list');
-    const listTwo = document.createElement('li');
-    listTwo.innerText='Two';
-    listTwo.classList.add('color');
-    
-    const listThree = document.createElement('li');
-    listThree.innerText='Three';
-    listThree.classList.add('color');
-
-    const listFour = document.createElement('li');
-    listFour.innerText='Four';
-    listFour.classList.add('color');
-
-    const listFive = document.createElement('li');
-    listFive.innerText='Five';
-    listFive.classList.add('color');
-    
-listElement.appendChild(listTwo);
-listElement.appendChild(listThree);
-listElement.appendChild(listFour);
-listElement.appendChild(listFive);
+const buttonClick=document.querySelector('btn');
+buttonClick.addEventListener('click', function(){
+    let userName = prompt('What is your name?');
+});
+alert(`Wellcome! ${userName}!`);
