e6cdce74569bf55ca05cfe90dda228024c822169
OxanaVoskresenskaya <oavoskresenskaya@mail.ru>
2022-05-30 17:50:20 +0300

    changing backgroundColor with button

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const buttonClick=document.querySelector('btn');
-buttonClick.addEventListener('click', function(){
-    let userName = prompt('What is your name?');
-});
-alert(`Wellcome! ${userName}!`);
+document.addEventListener('keydown', function(e){
+    if (e.key==='o') {
+    document.body.style.backgroundColor='red';
+    }else if (e.key==='x') {
+        document.body.style.backgroundColor='blue';
+    }else if (e.key==='a') {
+        document.body.style.backgroundColor='purple';
+    }else if (e.key==='n') {
+        document.body.style.backgroundColor='green';
+    }else {
+        document.body.style.backgroundColor='white';
+    }
+})
